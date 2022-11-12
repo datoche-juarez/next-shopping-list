84b67e5972e8108452433276f24081aa15e1ca67
Daniel Atoche-Juarez <datoche-juarez@Daniels-MacBook-Pro.local>
2022-11-12 04:17:18 -0500

    Very rough setup. Still need to come back and fix some thangs..

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,13 @@
-import '../styles/globals.css'
+// import '../styles/globals.css'
+import { ChakraProvider } from '@chakra-ui/react';
+import { newTheme } from '../styles/theme';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <ChakraProvider>
+   <Component {...pageProps} />
+    </ChakraProvider>
+  )
 }
 
 export default MyApp
---
+++ styles/components/buttonStyles.jsx
@@ -0,0 +1,80 @@
+import { whiten, darken, mode } from "@chakra-ui/theme-tools";
+
+export const ButtonStyles = {
+  // style object base or defualt style
+  baseStyle: {},
+  // styles for different sizes ("sm", "md", "lg")
+  sizes: {},
+  // styles for different visual variants ("outline", "solid")
+  variants: {
+    primary: () => ({
+      bg: "primary.main",
+      color: "white",
+      m: 2,
+      _hover: {
+        bg: "primary.200",
+        boxShadow: "md",
+      },
+    }),
+    secondary: () => ({
+      bg: "secondary.main",
+      color: "white",
+      m: 2,
+      _hover: {
+        bg: "secondary.200",
+        boxShadow: "md",
+      },
+    }),
+    secondaryOutline: () => ({
+      bg: "transparent",
+      border: "1px solid",
+      borderColor: "secondary.main",
+      color: "secondary.main",
+      m: 2,
+      _hover: {
+        boxShadow: "md",
+        transform: "scale(1.02)",
+      },
+    }),
+    highlight: () => ({
+      bg: "highlight.main",
+      color: "white",
+      m: 2,
+      _hover: {
+        bg: "highlight.200",
+        boxShadow: "md",
+      },
+    }),
+    highlightOutline: () => ({
+      bg: "transparent",
+      border: "1px solid",
+      borderColor: "highlight.500",
+      color: "highlight.500",
+      m: 2,
+      _hover: {
+        boxShadow: "md",
+        transform: "scale(1.02)",
+      },
+    }),
+    warning: () => ({
+      bg: "warning.main",
+      color: "white",
+      m: 2,
+      _hover: {
+        bg: "warning.200",
+        boxShadow: "md",
+      },
+    }),
+    danger: () => ({
+      bg: "danger.main",
+      color: "white",
+      m: 2,
+      _hover: {
+        bg: "danger.200",
+        boxShadow: "md",
+      },
+    }),
+  },
+  // default values for size and variant
+  defaultProps: {},
+};
