34557c51263df97315e6e6ea72245e737ab5e3bd
Daniel Atoche-Juarez <datoche-juarez@Daniels-MacBook-Pro.local>
2023-01-27 02:08:39 -0500

    Back links

--- pages/about.js
+++ pages/about.js
@@ -1,10 +1,12 @@
 import Meta from '../components/Meta'
+import Link from 'next/link'
 
 const about = () => {
     return (
         <div>
             <Meta title='About' />
             <h1>About</h1>
+            <Link href='/'>Go Back</Link>
         </div>
     )
 }
--- pages/vinyl.js
+++ pages/vinyl.js
@@ -1,10 +1,12 @@
 import Meta from '../components/Meta'
+import Link from 'next/link'
 
 const vinyl = () => {
     return (
         <div>
             <Meta title='Vinyl List' />
             <h1>Vinyl Shopping List</h1>
+            <Link href='/'>Go Back</Link>
         </div>
     )
 }
