88cc58496b2b656731d0641d69bc05b46742404d
Daniel Atoche-Juarez <datoche-juarez@Daniels-MacBook-Pro.local>
2023-02-02 02:04:43 -0500

    About pg and Vinyl pg changes...

--- pages/about.js
+++ pages/about.js
@@ -1,15 +1,32 @@
-import Meta from '../components/Meta'
-import Link from 'next/link'
+import Meta from "../components/Meta";
+import Link from "next/link";
 
+import {
+  Box,
+  Button,
+  Text,
+  Stack,
+} from "@chakra-ui/react";
 
 const about = () => {
-    return (
-        <div>
-            <Meta title='About' />
-            <h1>About</h1>
-            <Link href='/'>Go Back</Link>
-        </div>
-    )
-}
+  return (
+    <div as="flex" color="primary.main">
+      <Meta title="About" />
+      <Stack>
+        <Text as="b" fontSize="4xl">
+          About
+        </Text>
+        <Box boxShadow="xl" borderRadius="6" p="8" backgroundColor="bgGray.500">
+          <Text fontSize={{ sm: "xl", md: "2xl" }}>
+            About section description goes here...
+          </Text>
+          <Link href="/">
+            <Button variant="highlight">Go back</Button>
+          </Link>
+        </Box>
+      </Stack>
+    </div>
+  );
+};
 
-export default about
+export default about;
--- pages/vinyl.js
+++ pages/vinyl.js
@@ -1,12 +1,30 @@
 import Meta from '../components/Meta'
 import Link from 'next/link'
 
+import {
+    Box,
+    Button,
+    Text,
+    Stack,
+  } from "@chakra-ui/react";
+
 const vinyl = () => {
     return (
         <div>
             <Meta title='Vinyl List' />
-            <h1>Vinyl Shopping List</h1>
-            <Link href='/'>Go Back</Link>
+            <Stack>
+        <Text as="b" fontSize="4xl">
+          Vinyl List
+        </Text>
+        <Box boxShadow="xl" borderRadius="6" p="8" backgroundColor="bgGray.500">
+          <Text fontSize={{ sm: "xl", md: "2xl" }}>
+            Vinyl shopping list goes here...
+          </Text>
+          <Link href="/">
+            <Button variant="highlight">Go back</Button>
+          </Link>
+        </Box>
+      </Stack>
         </div>
     )
 }
