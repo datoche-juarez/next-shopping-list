b1643efc78599737a5e78b1df4493ef11e5bba48
Daniel Atoche-Juarez <datoche-juarez@Daniels-MacBook-Pro.local>
2023-02-08 19:20:00 -0500

    Updated cards on home page..

--- pages/index.js
+++ pages/index.js
@@ -12,24 +12,20 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to your <a href="https://nextjs.org">Vinyl Shopping List</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{" "}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
+        <div style={{ marginBottom: "50px" }}>
+          <h1 className={styles.title}>
+            Welcome to your <a href="https://nextjs.org">Vinyl Shopping List</a>
+          </h1>
+        </div>
         <div className={styles.grid}>
           <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
+            <h2>About &rarr;</h2>
+            <p>Find out more about this application.</p>
           </a>
 
           <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
+            <h2>Vinyl List &rarr;</h2>
+            <p>Get started editing your playlist now!</p>
           </a>
 
           <a
@@ -37,19 +33,7 @@ export default function Home() {
             className={styles.card}
           >
             <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
+            <p>See how others have used this tool.</p>
           </a>
         </div>
       </main>
--- pages/vinyl.js
+++ pages/vinyl.js
@@ -2,8 +2,41 @@ import Meta from "../components/Meta";
 import Link from "next/link";
 
 import { Box, Button, Text, Stack } from "@chakra-ui/react";
+import creds from "../creds.js";
 
-const vinyl = () => {
+const vinyl = ({ users }) => {
+  const clientId = creds.clientId;
+  const clientSecret = creds.clientSecret;
+  const userId = "1270083162";
+
+  // private methods
+  const _getToken = async () => {
+    const result = await fetch("https://accounts.spotify.com/api/token", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded",
+        Authorization: "Basic " + btoa(clientId + ":" + clientSecret),
+      },
+      body: "grant_type=client_credentials",
+    });
+
+    const data = await result.json();
+    console.log("Token: ", data.access_token);
+    console.log("getToken data: ", data);
+    return data.access_token;
+  };
+
+  const _getUserInfo = async (token, userId) => {
+    const result = await fetch(`https://api.spotify.com/v1/users/${userId}`, {
+      method: "GET",
+      headers: { Authorization: "Bearer " + token },
+    });
+
+    const data = await result.json();
+    console.log("getUserInfo data: ", data);
+    return data;
+  };
+
   return (
     <div>
       <Meta title="Vinyl List" />
@@ -15,6 +48,29 @@ const vinyl = () => {
           <Text fontSize={{ sm: "xl", md: "2xl" }}>
             Vinyl shopping list goes here...
           </Text>
+          <h1>List of users</h1>
+          {users.map((user) => {
+            return (
+              <div key={user.id}>
+                <p>{user.name}</p>
+                <p>{user.email}</p>
+              </div>
+            );
+          })}
+          <Button
+            variant="primary"
+            //create onClick event to call getToken
+            onClick={_getToken}
+          >
+            Fetch token
+          </Button>
+          <Button
+            variant="primary"
+            //create onClick event to call getUserInfo
+            onClick={_getUserInfo}
+          >
+            Fetch user info
+          </Button>
           <Link href="/">
             <Button variant="highlight">Go back</Button>
           </Link>
@@ -25,3 +81,15 @@ const vinyl = () => {
 };
 
 export default vinyl;
+
+export async function getStaticProps() {
+  const response = await fetch("https://jsonplaceholder.typicode.com/users");
+  const data = await response.json();
+  console.log(data);
+
+  return {
+    props: {
+      users: data,
+    },
+  };
+}
