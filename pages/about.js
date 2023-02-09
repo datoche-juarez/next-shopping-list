b607fb4aabf263118f3f53f31f8d56af03019651
Daniel Atoche-Juarez <datoche-juarez@Daniels-MacBook-Pro.local>
2023-02-09 07:07:31 -0500

    Added copy content for about section..

--- pages/about.js
+++ pages/about.js
@@ -12,8 +12,31 @@ const about = () => {
           About
         </Text>
         <Box boxShadow="xl" borderRadius="6" p="8" backgroundColor="bgGray.500">
-          <Text fontSize={{ sm: "xl", md: "2xl" }}>
-            About section description goes here...
+          <Text fontSize={{ sm: "md", md: "xl" }}>
+            Introducing the ultimate tool for music producers and playlist
+            curators. With or interactive Spotify playlist tool, you can easily
+            keep track of albums you want to purchase. Simply create a playlist
+            of songs that you want to sample, and our tool will take care of the
+            rest.
+            <br />
+            <br />
+            No more tedious manual editing or lost lists. Our tool will
+            duplicate your playlist and turn it into a shopping wishlist that
+            can be easily updated as you purchase albums. Mark off the albums
+            you've bought, and keep your original playlist intact.
+            <br />
+            <br />
+            Our user-friendly interface makes it easy to keep track of your
+            progress and stay organized. Whether you're a professional music
+            producers or just someone who loves creating playlists, our tool is
+            the perfect solution for streamlining your workflow and keeping your
+            music collection up-to-date.
+            <br />
+            <br />
+            Get started now and streamline your music production process like
+            never before!
+            <br />
+            <br />
           </Text>
           <Link href="/">
             <Button variant="highlight">Go back</Button>
