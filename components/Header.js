4f58f8077a19e39905049135a946a886f3d683e7
Daniel Atoche-Juarez <datoche-juarez@Daniels-MacBook-Pro.local>
2023-02-19 22:27:14 -0500

    Styling changes on home page (move into chakra theme later**)..

--- components/Header.js
+++ components/Header.js
@@ -1,8 +1,11 @@
+import styles from "../styles/Home.module.css";
+
 const Header = () => {
     return (
-        <div style={{ padding: "10px", textAlign: "center" }}>
+        // <div style={{ padding: "10px", textAlign: "center" }}>
+        <div className={styles.header}>
             <h1>
-                <span>Vinyl Shopping List</span>
+                <span>⚡️ Vinyl Shopping List ⚡️</span>
             </h1>
             <p>An interactive tool for Spotify playlists.</p>
         </div>
